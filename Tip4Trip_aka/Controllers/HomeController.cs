34785ede670d35c5d5a793662c2c5b38e18c6fe2
muntan <muntan@MUNTAN-PC>
2019-05-05 19:53:50 +0300

    kyriakh

--- Tip4Trip_aka/Controllers/HomeController.cs
+++ Tip4Trip_aka/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
 using Tip4Trip_aka.Models;
+using Tip4Trip_aka.ViewModels;
 
 namespace Tip4Trip_aka.Controllers
 {
@@ -12,11 +13,20 @@ namespace Tip4Trip_aka.Controllers
     public class HomeController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-
-        public ActionResult Index(string searching, string Address)
+       
+        public ActionResult Index(string searching, string Address,DateTime? Sstartdate,DateTime? Enddate)
         {
+            var Hous = db.Houses.Include(xxx => xxx.Reservations).Include(mmn => mmn.Location).Where(x => x.Location.NameCity.Contains(searching) && x.Address.Contains(Address));
            
-            return View(db.Houses.Include(mmn => mmn.Location).Where(x => x.Location.NameCity.Contains(searching) && x.Address.Contains(Address) || searching == null).ToList());
+            var res = db.Reservations.Where(c=>(c.StartDate <= Sstartdate )&& (c.EndDate<=Sstartdate));
+            var res2 = db.Reservations.Where(f => (f.EndDate >= Enddate )&& (f.StartDate >= Enddate));
+            var res3 = res.Concat(res2);
+            if (Sstartdate == null && Enddate == null) {  res3 = db.Reservations; }
+            
+            
+           HousesDates to_search_mas = new HousesDates { houses = Hous.ToList(), reservations = res3.ToList() };
+            if (to_search_mas == null) { return View(); }
+            return View(to_search_mas );
 
             //return View();
         }
---
+++ Tip4Trip_aka/ViewModels/HousesDates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tip4Trip_aka.Models;
+
+namespace Tip4Trip_aka.ViewModels
+{
+    public class HousesDates
+    {
+        public List<House> houses { get; set; }
+
+        public List<Reservation> reservations { get; set; }
+    }
+}
